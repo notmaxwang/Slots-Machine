6fce66f389e12c2e94edc2d6a696d808e2194f60
notmaxwang <maxwang3737@gmail.com>
2022-10-06 15:59:49 -0700

    added html, css files

---
+++ main.js
@@ -0,0 +1,25 @@
+//Constants
+
+const COLORS = {
+    '0': 'red',
+    '1': 'orange',
+    '2': 'yellow',
+    '3': 'green',
+    '4': 'blue',
+    '5': 'indigo',
+    '6': 'violet'
+}
+
+const REELSIZE = 3;
+
+const WINSTATE = [[0,0,0], 
+                 [1,1,1],
+                 [2,2,2],
+                 [3,3,3],
+                 [4,4,4],
+                 [5,5,5],
+                 [6,6,6]
+];
+
+
+
